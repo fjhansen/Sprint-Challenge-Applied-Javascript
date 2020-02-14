49276e0a6007024f1ae89861473ec3429a9968bc
Frances Hansen <francesjuniperhansen@gmail.com>
2020-02-14 08:55:09 -0800

    cards structure

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,39 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+let cardsContainer = document.querySelector('.cards-container')
+let two = "https://lambda-times-backend.herokuapp.com/articles"
+
+axios
+.get(two)
+.then(response => {
+    response.data.articles.javascript.forEach(item => {
+        
+        let javascript = cardMaker(item)
+        cardsContainer.appendChild(javascript);
+})
+})
+.catch(error => console.error(`Error: ${error}`))
+
+function cardMaker(obj) {
+    
+    let div1 = document.createElement('div');
+    let div2 = document.createElement('div');
+    let div3 = document.createElement('div');
+    let div4 = document.createElement('div');
+    let img1 = document.createElement('img');
+    let span = document.createElement('span');
+    
+    // Class
+    div1.classList.add('card');
+    div2.classList.add('headline');
+    // Text
+    div2.textContent = obj.headline;
+    img1.src = obj.authorPhoto;
+
+    // Append
+    cardsContainer.appendChild(div1);
+    div1.appendChild(div2);
+    // Return
+}
