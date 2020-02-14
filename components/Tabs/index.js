4e903be437bd34fcf00d4abf55562fc7b65e7f16
Frances Hansen <francesjuniperhansen@gmail.com>
2020-02-14 08:42:44 -0800

    tabs structure

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,17 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+let topics = document.querySelector('.topics')
+let one = "https://lambda-times-backend.herokuapp.com/topics"
+
+axios
+.get(one)
+.then({
+
+})
+.catch(())
+
+function Tabs() {
+
+}
